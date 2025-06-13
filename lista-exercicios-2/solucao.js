12a2082530d74dcefea4e11e1a43cc64d9e547fd
Thyago Chrystiann <thyago.chrystiann@hotmail.com>
2025-06-13 14:39:54 -0300

    Implementando as soluções das questões 4, 8 e 9.

--- lista-exercicios-2/solucao.js
+++ lista-exercicios-2/solucao.js
@@ -37,8 +37,7 @@ function questao2() {
  * 3. Dada uma string (ex.: "olá olá mundo mundo"), use if/else e for  
  * para extrair todas as palavras únicas e exibi-las em um array.
  */
-function questao3() {
-    const string = "olá olá mundo mundo hello world";
+function questao3(string="olá olá mundo mundo") {
     const palavrasUnicas = new Array();
     let palavra = "";
 
@@ -53,6 +52,7 @@ function questao3() {
             palavra = "";
         }
     }
+
     console.log(palavrasUnicas);
 }
 
@@ -60,7 +60,15 @@ function questao3() {
  * 4. Implemente function fatorial(n) de forma recursiva;  
  * trate n < 0 lançando um Error, e n === 0 retornando 1.
  */
- function questao4() {}
+ function questao4(numero) {
+    if (numero < 0 ) {
+        throw new Error("O número precisa ser maior do que 0.");
+    }
+    if (numero === 0) {
+        return 1;
+    }
+    return numero * questao4(numero - 1);
+ }
 
 /**
  * 5. Crie function debounce(fn, delay) que receba uma função fn e um delay em ms,  
@@ -81,40 +89,73 @@ function questao6() {}
  */
 function questao7() {
     const produtos = [
-        {nome: "Produto 1", valor: 5},
-        {nome: "Produto 2", valor: 2},
-        {nome: "Produto 3", valor: 9},
-        {nome: "Produto 4", valor: 7},
-        {nome: "Produto 5", valor: 3}];
+        {nome: "Produto 1", preco: 5},
+        {nome: "Produto 2", preco: 2},
+        {nome: "Produto 3", preco: 9},
+        {nome: "Produto 4", preco: 7},
+        {nome: "Produto 5", preco: 3},
+    ];
 
     function compararPreco(produto1, produto2) {
-        return produto1.valor - produto2.valor;
+        return produto1.preco - produto2.preco;
     }
 
-    return produtos
-    .sort(compararPreco)
-    .map(produto => produto.nome);
+    return produtos.sort(compararPreco).map(produto => produto.nome);
 }
 
 /**
  * 8. Em vendas = [{ cliente, total }, …], use reduce para gerar um objeto onde
  * cada chave é um cliente e o valor é a soma de todos os seus total.
  */
-function questao8() {}
+function questao8() {
+    const vendas = [
+        {cliente: "Cliente 1", total: 1},
+        {cliente: "Cliente 2", total: 2},
+        {cliente: "Cliente 1", total: 3},
+        {cliente: "Cliente 3", total: 4},
+        {cliente: "Cliente 2", total: 5},
+    ];
+
+    function acumularVendas(acumulador, venda) {
+        acumulador[venda.cliente] = (acumulador[venda.cliente] || 0) + venda.total;
+        return acumulador;
+    }
+
+    return vendas.reduce(acumularVendas, {});
+}
 
 /**
  * 9. Escreva duas funções:  
  * ○ paresParaObjeto(pares) recebe um array de pares [ [chave, valor], … ] e retorna o objeto equivalente.  
  * ○ objetoParaPares(obj) faz o inverso, retornando um array de pares.
  */
-function questao9() {}
+function questao9() {
+    function paresParaObjeto(acumulador, par) {
+        acumulador[par[0]] = par[1];
+        return acumulador;
+    }
+
+    function objetoParaPares(objeto) {
+        const pares = new Array();
+        for (const key in objeto) {
+            pares.push([key, objeto[key]]);
+        }
+        return pares;
+    }
+
+    pares = [["Chave 1", 1], ["Chave 2", 2], ["Chave 3", 3]];
+    objeto = {"Chave 1": 1, "Chave 2": 2, "Chave 3": 3};
+
+    console.log(pares.reduce(paresParaObjeto, {}));
+    console.log(objetoParaPares(objeto));
+}
 
 // questao1();
 // questao2();
-// questao3();
-// questao4();
+// questao3("olá olá mundo mundo hello world");
+// console.log(questao4(5));
 // questao5();
 // questao6();
-console.log(questao7());
+// console.log(questao7());
 // questao8();
 // questao9();
