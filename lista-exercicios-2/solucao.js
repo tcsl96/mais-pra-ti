03eb4bfb2d17878c85f7be4b242a721dae7074fc
Thyago Chrystiann <thyago.chrystiann@hotmail.com>
2025-06-13 00:43:06 -0300

    Implementando as soluções das questões 2, 3 e 7. Além disso, ajustando o package.lock dos diretórios.

--- lista-exercicios-2/solucao.js
+++ lista-exercicios-2/solucao.js
@@ -13,13 +13,48 @@ function questao1() {}
  * Use while para repetir até acertar, contando tentativas  
  * e exibindo “mais alto” ou “mais baixo” a cada palpite errado.
  */
-function questao2() {}
+function questao2() {
+    const numeroSorteado = Math.floor(Math.random() * 100 + 1);
+    let escolha = 0;
+    let tentativas = 1;
+
+    console.log("Um número de 1 a 100 foi sorteado.");
+    while (true) {
+        escolha = Number(input("Qual número você acha que é? "));
+        if (escolha == numeroSorteado) {
+            console.log(`Parabéns! Você acertou com ${tentativas} tentativa(s).`);
+            break;
+        } else if (escolha > numeroSorteado) {
+            console.log("Mais baixo. O número de tentativas é:", tentativas);
+        } else {
+            console.log("Mais alto. O número de tentativas é:", tentativas);
+        }
+        tentativas++;
+    }
+}
 
 /**
  * 3. Dada uma string (ex.: "olá olá mundo mundo"), use if/else e for  
  * para extrair todas as palavras únicas e exibi-las em um array.
  */
-function questao3() {}
+function questao3() {
+    const string = "olá olá mundo mundo hello world";
+    const palavrasUnicas = new Array();
+    let palavra = "";
+
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] != " ") {
+            palavra += string[i];
+        } 
+        if (string[i] == " " || i == (string.length - 1)) {
+            if (!palavrasUnicas.includes(palavra)) {
+                palavrasUnicas.push(palavra);
+            }
+            palavra = "";
+        }
+    }
+    console.log(palavrasUnicas);
+}
 
 /**
  * 4. Implemente function fatorial(n) de forma recursiva;  
@@ -44,7 +79,22 @@ function questao6() {}
  * crie uma função que retorne um novo array apenas com os nomes,  
  * ordenados por preço crescente, usando map, sort.
  */
-function questao7() {}
+function questao7() {
+    const produtos = [
+        {nome: "Produto 1", valor: 5},
+        {nome: "Produto 2", valor: 2},
+        {nome: "Produto 3", valor: 9},
+        {nome: "Produto 4", valor: 7},
+        {nome: "Produto 5", valor: 3}];
+
+    function compararPreco(produto1, produto2) {
+        return produto1.valor - produto2.valor;
+    }
+
+    return produtos
+    .sort(compararPreco)
+    .map(produto => produto.nome);
+}
 
 /**
  * 8. Em vendas = [{ cliente, total }, …], use reduce para gerar um objeto onde
@@ -65,6 +115,6 @@ function questao9() {}
 // questao4();
 // questao5();
 // questao6();
-// questao7();
+console.log(questao7());
 // questao8();
 // questao9();
